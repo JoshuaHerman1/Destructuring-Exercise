99fb61a67505da5547b64af421b23701c1ae5f3a
JoshuaHerman1 <joshua.herman.biz@gmail.com>
2023-01-05 16:09:11 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//q1
+8
+1846
+
+//q2
+{yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+//q3
+Your name is Alejandro and you like purple
+
+Your name is Melissa and you like green
+
+Your name is undefined and you like green
+
+//q4
+Maya 
+Marisa
+Chi 
+
+//q5
+"Raindrops on roses"
+"whiskers on kittens"
+["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+//q6
+[10,30,20]
+
+//q7
+const obj = {
+  numbers: {
+    a: 1,
+    b: 2
+  }
+};
+
+const {a,b} = obj.numbers
+
+//q8
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+//q8
+{first: "Tom", second: "Margaret", third: "Allison", rest: ["David", "Pierre"]}
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
